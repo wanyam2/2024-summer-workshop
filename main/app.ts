6b8f9cc035758daa5317c1d1a47864cc56a07254
EUNCHAE <2snha@naver.com>
2024-09-01 15:34:55 +0900

    html

--- main/app.ts
+++ main/app.ts
@@ -4,7 +4,28 @@ const server = Bun.serve({
     const path = new URL(req.url).pathname;
 
     if (path === "/") {
-      return new Response("경 2024 신한 SOL 뱅크 KBO리그 챔피언 LG TWINS 축");
+      const html = `
+      <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>한밭대학교 무선통신연구실</h2>
+    <h3>이은채</h3>
+    <h4>너무재밋 당!!!!!!!!!!!!!!!!!</h4>
+
+</body>
+</html>`;
+      return new Response(html, {
+        headers: {
+          "content-type": "text/html",
+        },
+      });
     }
 
     return new Response("Not found", { status: 404 });
