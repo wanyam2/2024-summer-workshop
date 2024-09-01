539ce1600736b386269d9b98ba59fc091c8ba47a
EUNCHAE <2snha@naver.com>
2024-09-01 15:38:08 +0900

    html

--- main/app.ts
+++ main/app.ts
@@ -5,22 +5,10 @@ const server = Bun.serve({
 
     if (path === "/") {
       const html = `
-      <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
     <h2>한밭대학교 무선통신연구실</h2>
     <h3>이은채</h3>
     <h4>너무재밋 당!!!!!!!!!!!!!!!!!</h4>
-
-</body>
-</html>`;
+`;
       return new Response(html, {
         headers: {
           "content-type": "text/html",
